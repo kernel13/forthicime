485250b68133ed9c3b6ddad16ebf8be608cf48a1
busyboard <contact@bb.c-coool.com>
2013-07-08 18:59:19 +0200

    .

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -2,44 +2,44 @@
 # =============================================================================
 # REQUIRED VARIABLES
 # =============================================================================
-set :application, "Forthicime"
-#set :domain,      "#{application}.com"
-set :deploy_to,   "/kunden/homepages/32/d299567504/htdocs/laboratoire-marachlian/forthicime"
+set :application, 		"Forthicime"
+#set :domain,      		"#{application}.com"
+set :deploy_to,   		"/kunden/homepages/32/d299567504/htdocs/laboratoire-marachlian/forthicime"
 
 
 # =============================================================================
 # SCM OPTIONS
 # =====================================================================
-set :repository,  "git@github.com:kernel13/forthicime.git"
-set :scm,         :git
-set :scm_user, "kernel13"
-set :scm_password, "ciotat13"
-set :branch, 'master' 
-set :scm_verbose, true
+set :repository,  		"git@github.com:kernel13/forthicime.git"
+set :scm,         		:git
+set :scm_user, 			"kernel13"
+set :scm_password, 		"ciotat13"
+set :branch, 			"master"
+set :scm_verbose, 		true
 
 # =============================================================================
 # SSH OPTIONS
 # =============================================================================
-set :user, "u55016233"
-set :use_sudo, false
+set :user,		 		"u55016233"
+set :use_sudo, 			false
 
 
 # =============================================================================
 # ROLES
 # =============================================================================
-role :web,        "laboratoire-marachlian.fr" 	                        # Your HTTP server, Apache/etc
-role :app,        "laboratoire-marachlian.fr", :primary => true       # This may be the same as your `Web` server
+role :web,        		"laboratoire-marachlian.fr" 	                        # Your HTTP server, Apache/etc
+role :app,        		"laboratoire-marachlian.fr", :primary => true       # This may be the same as your `Web` server
 
 # =============================================================================
 # CAPISTRANO OPTIONS
 # =============================================================================
-set  :keep_releases,  3
-set  :deploy_via, :copy
+set  :keep_releases,  	3
+set  :deploy_via, 		:copy
 
 # =============================================================================
 # Doctrine
 # =============================================================================
-set :model_manager, "doctrine"
+set :model_manager, 	"doctrine"
 
 # =============================================================================
 # LOG LEVEL
